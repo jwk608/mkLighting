30e6a060bf1c274b91dc5bc5197d3376b950b5c4
Joong <joongwonkim608@gmail.com>
2020-01-27 23:52:57 -0500

    lazy load all

--- wp-content/themes/oceanwp-child/templates/gallery.php
+++ wp-content/themes/oceanwp-child/templates/gallery.php
@@ -35,31 +35,29 @@ get_header();
     <img class="lazy" data-src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(133).jpg " width="640" height="480"x class="element-item transition metal">
   </div>
   <div class="photo-column">
-    <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(117).jpg" style="width:100%">
-    <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(98).jpg" style="width:100%">
-    <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(131).jpg" style="width:100%">
-    <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(123).jpg" style="width:100%">
-    <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(118).jpg" style="width:100%">
-    <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(128).jpg" style="width:100%">
-    <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(133).jpg " style="width:100%">
+    <img class="lazy"src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(117).jpg" style="width:100%">
+    <img class="lazy "src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(98).jpg" style="width:100%">
+    <img class="lazy" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(123).jpg" style="width:100%">
+    <img class="lazy "src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(118).jpg" style="width:100%">
+    <img class="lazy "src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(128).jpg" style="width:100%">
+    <img class="lazy "src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(133).jpg " style="width:100%">
   </div>  
   <div class="photo-column">
-    <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(117).jpg" style="width:100%">
-    <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(98).jpg" style="width:100%">
-    <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(131).jpg" style="width:100%">
-    <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(123).jpg" style="width:100%">
-    <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(118).jpg" style="width:100%">
-    <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(128).jpg" style="width:100%">
-    <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(133).jpg " style="width:100%">
+    <img class="lazy"src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(117).jpg" style="width:100%">
+    <img class="lazy" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(131).jpg" style="width:100%">
+    <img class="lazy "src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(123).jpg" style="width:100%">
+    <img class="lazy "src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(118).jpg" style="width:100%">
+    <img class="lazy "src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(128).jpg" style="width:100%">
+    <img class="lazy "src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(133).jpg " style="width:100%">
   </div>
   <div class="photo-column">
-    <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(117).jpg" style="width:100%">
-    <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(98).jpg" style="width:100%">
-    <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(131).jpg" style="width:100%">
-    <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(123).jpg" style="width:100%">
-    <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(118).jpg" style="width:100%">
-    <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(128).jpg" style="width:100%">
-    <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(133).jpg " style="width:100%">
+    <img class="lazy "src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(117).jpg" style="width:100%">
+    <img class="lazy "src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(98).jpg" style="width:100%">
+    <img class="lazy "src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(131).jpg" style="width:100%">
+    <img class="lazy "src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(123).jpg" style="width:100%">
+    <img class="lazy "src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(118).jpg" style="width:100%">
+    <img class="lazy "src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(128).jpg" style="width:100%">
+    <img class="lazy "src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(133).jpg " style="width:100%">
   </div>
 </div>
     <button data-tippy-content="Tooltip">Text</button>
