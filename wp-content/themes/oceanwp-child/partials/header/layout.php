da045047cb7bd3e4bc18b52e9a58f93d032cc27f
Joong <joongwonkim608@gmail.com>
2020-02-05 23:52:08 -0500

    reformat code

--- wp-content/themes/oceanwp-child/partials/header/layout.php
+++ wp-content/themes/oceanwp-child/partials/header/layout.php
@@ -6,7 +6,7 @@
  */
 
 // Exit if accessed directly
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
     exit;
 }
 ?>
@@ -17,32 +17,34 @@ if ( ! defined( 'ABSPATH' ) ) {
             data-aos-duration="200">
 
         <nav>
-            <div class="inline">            <a href="/" class="logo"><img src="http://mklighting.test:3001/wp-content/uploads/2020/02/MK-lighting-ff-01.jpg" alt=""></a>
+            <div class="inline"><a href="/" class="logo"><img
+                        src="http://mklighting.test:3001/wp-content/uploads/2020/02/MK-lighting-ff-01.jpg"
+                        alt=""></a>
             </div>
             <div class="inline">
-                <a href="<?php echo get_site_url()?>">HOME</a>
-                <a href="<?php echo get_site_url()?>/about-us">ABOUT US</a>
-                <a href="<?php echo get_site_url()?>/gallery">SOLUTION</a>
-                <a href="<?php echo get_site_url()?>/contact-us" class="contact-us">CONTACT US</a>
+                <a href="<?php echo get_site_url() ?>">HOME</a>
+                <a href="<?php echo get_site_url() ?>/about-us">ABOUT US</a>
+                <a href="<?php echo get_site_url() ?>/gallery">SOLUTION</a>
+                <a href="<?php echo get_site_url() ?>/contact-us" class="contact-us">CONTACT US</a>
             </div>
 
         </nav>
 
     </header>
 
-<!--    <nav>-->
-<!--        <div class="logo">-->
-<!--            <img src="logo.png" alt="" class="logo">-->
-<!--        </div>-->
-<!--        <ul>-->
-<!--            <li><a href="--><?php //echo esc_url( home_url( '/' ) ); ?><!--" class="active">Home</a></li>-->
-<!--            <li><a href="/about-us">About</a></li>-->
-<!--            <li><a href="/gallery">Gallery</a></li>-->
-<!--            <li><a href="/contact">Contact</a></li>-->
-<!--            <li><a href="/services">Services</a></li>-->
-<!--        </ul>-->
-<!--    </nav>-->
+    <!--    <nav>-->
+    <!--        <div class="logo">-->
+    <!--            <img src="logo.png" alt="" class="logo">-->
+    <!--        </div>-->
+    <!--        <ul>-->
+    <!--            <li><a href="--><?php //echo esc_url( home_url( '/' ) ); ?><!--" class="active">Home</a></li>-->
+    <!--            <li><a href="/about-us">About</a></li>-->
+    <!--            <li><a href="/gallery">Gallery</a></li>-->
+    <!--            <li><a href="/contact">Contact</a></li>-->
+    <!--            <li><a href="/services">Services</a></li>-->
+    <!--        </ul>-->
+    <!--    </nav>-->
 
 <?php
 
-do_action( 'ocean_after_header' ); ?>
+do_action('ocean_after_header'); ?>
