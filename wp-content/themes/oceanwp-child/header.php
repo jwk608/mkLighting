cddcf834aee2439dfa897f7d74d9a5a171f4a42a
Joong <joongwonkim608@gmail.com>
2020-06-29 22:31:20 -0400

    add google tag manager

--- wp-content/themes/oceanwp-child/header.php
+++ wp-content/themes/oceanwp-child/header.php
@@ -8,6 +8,21 @@
     <!DOCTYPE html>
 <html class="<?php echo esc_attr( oceanwp_html_classes() ); ?>" <?php language_attributes(); ?><?php oceanwp_schema_markup( 'html' ); ?>>
     <head>
+        <!-- Google Tag Manager -->
+        <script>(function (w, d, s, l, i) {
+                w[l] = w[l] || [];
+                w[l].push({
+                    'gtm.start':
+                        new Date().getTime(), event: 'gtm.js'
+                });
+                var f = d.getElementsByTagName(s)[0],
+                    j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+                j.async = true;
+                j.src =
+                    'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+                f.parentNode.insertBefore(j, f);
+            })(window, document, 'script', 'dataLayer', 'GTM-N4RZQ9J');</script>
+        <!-- End Google Tag Manager -->
         <meta charset="<?php bloginfo( 'charset' ); ?>">
         <link rel="profile" href="http://gmpg.org/xfn/11">
         <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
@@ -15,7 +30,12 @@
     </head>
 
 <body <?php body_class(); ?>>
-
+    <!-- Google Tag Manager (noscript) -->
+    <noscript>
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N4RZQ9J"
+                height="0" width="0" style="display:none;visibility:hidden"></iframe>
+    </noscript>
+    <!-- End Google Tag Manager (noscript) -->
 <?php do_action( 'ocean_before_outer_wrap' ); ?>
 
 <div id="outer-wrap" class="site clr">
